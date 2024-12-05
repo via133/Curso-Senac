9a52b91f21e99d108c0f2d851b67e8c729551c87
via133 <esterbezerra770@gmail.com>
2024-12-05 11:25:58 -0300

    Add files via upload

---
+++ Aula 2/aula02.py/aula02.py
@@ -0,0 +1,11 @@
+print("good morning")
+nome_usuario = "ester bezerra"#o que esta quardado aqui e uma texto
+idade_usuario=21#controle de dados 
+altura_usuario=1.60#tipo de dados para comtrole de infornacao  
+maio_idade=True#tipo bool booleari
+menor_idade=False
+print("seja bem vindo",nome_usuario)
+print("sua idade",idade_usuario 'anos')
+print("voce tem qual altura", altura_usuario'metros')
+print("voce e maoir de idede",maio_idade)
+print("voce e menor de idade",menor_idade)
